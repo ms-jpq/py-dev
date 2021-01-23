2b7e033d41ad3f0c2d2b1407a82366a11d871604
Fly me to the moon <git@bigly.dog>
2021-01-24 06:37:58 +0700

    ok

--- py_dev/http_echo/srv.py
+++ py_dev/http_echo/srv.py
@@ -2,41 +2,51 @@ from http.server import BaseHTTPRequestHandler
 from shutil import get_terminal_size
 
 
-def _log_req(handler: BaseHTTPRequestHandler) -> None:
-    cols, _ = get_terminal_size()
-    print("*" * cols)
+def _echo_req(handler: BaseHTTPRequestHandler) -> None:
+    """
+    Parse
+    """
 
     method = handler.command
     path = handler.path
     headers = handler.headers.items()
-
-    body_len = next(
+    content_len = next(
         (int(val) for key, val in headers if key.lower() == "content-length"), 0
     )
-    body = handler.rfile.read(body_len).decode()
+    raw = handler.rfile.read(content_len)
 
-    print(f"{method} {path}")
+    """
+    Reply
+    """
+
+    handler.send_response_only(200)
+    for key, val in handler.headers.items():
+        handler.send_header(key, val)
+    handler.end_headers()
+    handler.wfile.write(raw)
+
+    """
+    Log
+    """
+
+    cols, _ = get_terminal_size()
+    print("*" * cols)
+    print(f"{method.ljust(10)} {path}")
     print("::Headers::")
     for key, val in headers:
         print(f"{key}: {val}")
+    body = raw.decode(errors="replace")
     if body:
         print("::Body::")
         print(body)
 
 
-def _resp(handler: BaseHTTPRequestHandler) -> None:
-    handler.send_response_only(200)
-    handler.end_headers()
-    handler.wfile.write(b"OK")
-
-
 class EchoServer(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
-        _log_req(self)
-        _resp(self)
+        _echo_req(self)
 
     def do_POST(self) -> None:
-        self.do_GET()
+        _echo_req(self)
 
     def do_PUT(self) -> None:
-        self.do_POST()
+        _echo_req(self)
