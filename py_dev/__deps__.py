cd65c969afd87d6b916d96483fb9e6b3f3706bc6
Fly me to the moon <git@bigly.dog>
2021-01-24 06:01:11 +0700

    ci

---
+++ py_dev/__deps__.py
@@ -0,0 +1,44 @@
+from os import environ
+from pathlib import Path
+from shutil import which
+from subprocess import DEVNULL, run
+from sys import argv, path, stderr
+
+_REQUIREMENTS = Path(__file__).parent / "requirements.text"
+_XDG_DATA_HOME = environ.get("XDG_DATA_HOME")
+_RT_DIR = (
+    (Path(_XDG_DATA_HOME) if _XDG_DATA_HOME else Path().home() / ".local" / "share")
+    / "py_dev"
+    / "runtime"
+)
+_RT_DIR.mkdir(parents=True, exist_ok=True)
+path.append(str(_RT_DIR))
+
+
+try:
+    import pygments
+    import std2
+except ImportError:
+    cmd = "pip3"
+    if which(cmd):
+        proc = run(
+            (
+                cmd,
+                "install",
+                "--upgrade",
+                "--target",
+                str(_RT_DIR),
+                "--requirement",
+                str(_REQUIREMENTS),
+            ),
+            cwd=_RT_DIR,
+            stdin=DEVNULL,
+            stdout=stderr,
+        )
+        if proc.returncode:
+            exit(proc.returncode)
+        else:
+            proc = run(argv)
+            exit(proc.returncode)
+    else:
+        raise
---
+++ py_dev/ccat/__main__.py
@@ -0,0 +1,61 @@
+from argparse import ArgumentParser, Namespace
+from sys import stdin
+from typing import Optional
+from pathlib import Path
+
+from pygments import highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.formatters._mapping import FORMATTERS
+from pygments.lexer import Lexer
+from pygments.lexers import get_lexer_for_filename, guess_lexer
+from pygments.lexers.special import TextLexer
+from pygments.styles import get_all_styles, get_style_by_name
+from pygments.util import ClassNotFound
+
+
+def _arg_parse() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("name", nargs=None if stdin.isatty() else "?")
+    parser.add_argument("-", dest="use_stdin", action="store_true")
+
+    themes = sorted(get_all_styles())
+    parser.add_argument("-t", "--theme", choices=themes, default="friendly")
+
+    formatters = sorted(
+        name for _, _, names, *_ in FORMATTERS.values() for name in names
+    )
+    parser.add_argument("-f", "--formatter", choices=formatters, default="terminal16m")
+
+    return parser.parse_args()
+
+
+def _slurp(name: Optional[str], use_stdin: bool) -> str:
+    if use_stdin or name is None:
+        return stdin.read()
+    else:
+        return Path(name).read_text()
+
+def _get_lexer(file_name: Optional[str], text: str) -> Lexer:
+    try:
+        return get_lexer_for_filename(file_name)
+    except (ClassNotFound, TypeError):
+        try:
+            return guess_lexer(text)
+        except ClassNotFound:
+            return TextLexer()
+
+
+def main() -> None:
+    args = _arg_parse()
+    text = _slurp(args.name, args.use_stdin)
+
+    style = get_style_by_name(args.theme)
+    formatter = get_formatter_by_name(args.formatter, style=style)
+
+    lexer = _get_lexer(args.name, text)
+    pretty = highlight(text, lexer=lexer, formatter=formatter)
+
+    print(pretty, end="")
+
+
+main()
