bb573e7e01ca9994c61cbac9de979191e45d0093
Fly me to the moon <git@bigly.dog>
2021-01-24 06:43:54 +0700

    lbda

--- py_dev/http_echo/srv.py
+++ py_dev/http_echo/srv.py
@@ -1,44 +1,41 @@
 from http.server import BaseHTTPRequestHandler
+from locale import strxfrm
 from shutil import get_terminal_size
+from typing import Mapping
+
+
+def _log(method: str, path: str, headers: Mapping[str, str], content: bytes) -> None:
+    cols, _ = get_terminal_size()
+    print("*" * cols)
+    print(f"{method.ljust(10)} {path}")
+    print("::Headers::")
+    for key, val in headers.items():
+        print(f"{key}: {val}")
+    body = content.decode(errors="replace")
+    if body:
+        print("::Body::")
+        print(body)
 
 
 def _echo_req(handler: BaseHTTPRequestHandler) -> None:
-    """
-    Parse
-    """
-
-    method = handler.command
-    path = handler.path
     headers = handler.headers.items()
     content_len = next(
         (int(val) for key, val in headers if key.lower() == "content-length"), 0
     )
-    raw = handler.rfile.read(content_len)
-
-    """
-    Reply
-    """
+    content = handler.rfile.read(content_len)
 
     handler.send_response_only(200)
     for key, val in handler.headers.items():
         handler.send_header(key, val)
     handler.end_headers()
-    handler.wfile.write(raw)
+    handler.wfile.write(content)
 
-    """
-    Log
-    """
-
-    cols, _ = get_terminal_size()
-    print("*" * cols)
-    print(f"{method.ljust(10)} {path}")
-    print("::Headers::")
-    for key, val in headers:
-        print(f"{key}: {val}")
-    body = raw.decode(errors="replace")
-    if body:
-        print("::Body::")
-        print(body)
+    _log(
+        method=handler.command,
+        path=handler.path,
+        headers={k: v for k, v in sorted(headers, key=lambda t: strxfrm(t[0]))},
+        content=content,
+    )
 
 
 class EchoServer(BaseHTTPRequestHandler):
