ee80899cebc662858855b85c9b4b3c684326316b
Fly me to the moon <git@bigly.dog>
2021-01-24 05:48:40 +0700

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages(exclude=("tests*",))
+package_data = {pkg: ("py.typed", "requirements.txt") for pkg in packages}
+scripts = listdir("scripts")
+
+
+setup(
+    name="py_dev",
+    python_requires=">=3.8.0",
+    version="0.1.0",
+    description="python dev tools",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/py-dev",
+    scripts=scripts,
+    packages=packages,
+    package_data=package_data,
+)
