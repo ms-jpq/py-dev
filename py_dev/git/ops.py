55929ae29cf82be13a8c350cb39170d2f6a6ee7c
Fly me to the moon <git@bigly.dog>
2021-01-27 05:21:06 -0800

    fix print

--- py_dev/git/ops.py
+++ py_dev/git/ops.py
@@ -13,7 +13,7 @@ from ..ccat.pprn import pprn_basic
 
 def print_git_show(sha: str, path: str) -> None:
     end = linesep if stdout.isatty() else ""
-    cmd = join(("git", "show", f"{sha}~:{path}"))
+    cmd = join(("git", "show", f"{sha}:{path}"))
     print(cmd, end=end)
 
 
