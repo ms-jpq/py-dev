183c16bc6c0d53402568f7dcf546cf154979b6c0
Fly me to the moon <git@bigly.dog>
2021-01-29 23:45:47 -0800

    mv

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 packages = find_packages(exclude=("tests*",))
-package_data = {pkg: ("py.typed", "requirements.txt") for pkg in packages}
+package_data = {pkg: ("py.typed",) for pkg in packages}
 scripts = tuple(map(str, Path("scripts").iterdir()))
+install_requires = Path("requirements.txt").read_text().splitlines()
 
 
 setup(
     name="py_dev",
     python_requires=">=3.8.0",
+    install_requires=install_requires,
     version="0.1.0",
     description="python dev tools",
     long_description=Path("README.md").read_text(),
