d523fe5848bd3db52d6e95ed01ded15d97fbc53b
Fly me to the moon <git@bigly.dog>
2021-04-20 21:28:58 -0700

    ok

--- py_dev/srv/static.py
+++ py_dev/srv/static.py
@@ -1,15 +1,18 @@
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
+from locale import strxfrm
 from mimetypes import guess_type
 from pathlib import Path, PurePosixPath
 from shutil import copyfileobj
-from typing import Optional
+from typing import Sequence, Tuple, Union
 from urllib.parse import urlsplit
 
 from std2.pathlib import is_relative_to
 
 
-def _seek(handler: BaseHTTPRequestHandler, assets_dir: Path) -> Optional[Path]:
+def _seek(
+    handler: BaseHTTPRequestHandler, assets_dir: Path
+) -> Union[Path, None, Sequence[Tuple[bool, Path]]]:
     uri = urlsplit(handler.path)
     path = PurePosixPath(uri.path)
     asset = (assets_dir / path).resolve()
@@ -17,11 +20,11 @@ def _seek(handler: BaseHTTPRequestHandler, assets_dir: Path) -> Optional[Path]:
     if not is_relative_to(asset, assets_dir) or not asset.exists():
         return None
     elif asset.is_dir():
-        index = asset / "index.html"
-        if not index.exists():
-            return None
-        else:
-            return index
+        paths = sorted(
+            ((path.is_dir(), path) for path in asset.iterdir()),
+            key=lambda p: (p[0], strxfrm(str(p[1]))),
+        )
+        return paths
     else:
         return asset
 
