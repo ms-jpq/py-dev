c98a87733adc677b6fc1a6a5316cb55ce8abfc51
DOG DOG DOG! <github@bigly.dog>
2021-07-09 08:49:53 -0700

    q

--- py_dev/git/do/__main__.py
+++ py_dev/git/do/__main__.py
@@ -5,8 +5,9 @@ from ...run import run_main
 
 
 def main() -> None:
-    check_call(("git", *argv))
-    check_call(("git", "submodules", "foreach", "--recursive", *argv))
+    args = argv[1:]
+    check_call(("git", *argv[1:]))
+    check_call(("git", "submodules", "foreach", "--recursive", *args))
 
 
 run_main(main)
