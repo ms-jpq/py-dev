820b04dadb64cc559bfd71ff006d21863979777d
DOG DOG DOG! <github@bigly.dog>
2021-07-09 08:13:34 -0700

    argv

--- py_dev/git/do/__main__.py
+++ py_dev/git/do/__main__.py
@@ -1,20 +1,12 @@
-from argparse import ArgumentParser, Namespace
 from subprocess import check_call
+from sys import argv
 
 from ...run import run_main
 
 
-def _parse_args() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument("args", nargs="+")
-    return parser.parse_args()
-
-
 def main() -> None:
-    args = _parse_args()
-    a = args.args
-    check_call(("git", *a))
-    check_call(("git", "submodules", "foreach", "--recursive", *a))
+    check_call(("git", *argv))
+    check_call(("git", "submodules", "foreach", "--recursive", *argv))
 
 
 run_main(main)
