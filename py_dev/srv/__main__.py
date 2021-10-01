5f5caebc2db2067f928a4044b7d8a88ed2617503
Fly me to the moon <git@bigly.dog>
2021-10-01 00:02:23 -0700

    ok

--- py_dev/srv/__main__.py
+++ py_dev/srv/__main__.py
@@ -1,10 +1,9 @@
 from argparse import ArgumentParser, Namespace
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-from os import curdir, linesep
+from os import curdir
 from os.path import normcase
 from pathlib import Path, PurePath
 from socket import getfqdn
-from typing import Any
 
 from std2.shutil import hr_print
 
@@ -14,8 +13,9 @@ from .static import build_j2, get, head
 
 
 def _parse_args() -> Namespace:
+    cwd = PurePath(normcase(curdir))
     parser = ArgumentParser()
-    parser.add_argument("root", nargs="?", type=PurePath, default=PurePath(curdir))
+    parser.add_argument("root", nargs="?", type=PurePath, default=cwd)
     parser.add_argument("-o", "--open", action="store_true")
     parser.add_argument("-p", "--port", type=int, default=8080)
     return parser.parse_args()
@@ -25,13 +25,11 @@ async def main() -> int:
     args = _parse_args()
     addr = "" if args.open else "localhost"
     bind = (addr, args.port)
+    host = getfqdn() if args.open else "localhost"
+
+    j2 = build_j2()
     try:
         root = Path(normcase(args.root)).resolve(strict=True)
-    except OSError as e:
-        log.fatal("%s", hr_print(f"{args.root}{linesep}{e}"))
-        return 1
-    else:
-        j2 = build_j2()
 
         class Handler(BaseHTTPRequestHandler):
             def do_HEAD(self) -> None:
@@ -40,14 +38,13 @@ async def main() -> int:
             def do_GET(self) -> None:
                 get(j2, handler=self, root=root)
 
-            def log_message(self, format: str, *args: Any) -> None:
-                pass
-
         httpd = ThreadingHTTPServer(bind, Handler)
-        host = getfqdn() if args.open else "localhost"
+    except OSError as e:
+        log.fatal("%s", hr_print(e))
+        return 1
+    else:
         log.info("%s", hr_print(f"SERVING -- http://{host}:{args.port}"))
         httpd.serve_forever()
-
         return 0
 
 
