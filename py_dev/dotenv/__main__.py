80188c167fb359055c2d7a4c8439382e91d30415
Fly me to the moon <git@bigly.dog>
2021-10-28 15:48:07 -0700

    nicer code

--- py_dev/dotenv/__main__.py
+++ py_dev/dotenv/__main__.py
@@ -1,20 +1,39 @@
 from argparse import ArgumentParser, Namespace
-from configparser import ConfigParser, ParsingError
+from configparser import ConfigParser
 from itertools import chain
 from os import environ, linesep
 from pathlib import Path
 from shlex import split
 from string import Template, ascii_letters, digits
 from sys import stderr, stdin, stdout
+from typing import Iterable, Iterator, Tuple
 from uuid import uuid4
 
-from ..log import log
 from ..run import run_main
 
 
-class _Parser(ConfigParser):
-    def optionxform(self, optionstr: str) -> str:
-        return optionstr
+def _parse(text: str) -> Iterator[Tuple[str, str]]:
+    class _Parser(ConfigParser):
+        def optionxform(self, optionstr: str) -> str:
+            return optionstr
+
+    lines = "".join(chain((f"[{uuid4()}]", linesep), text))
+    parser = _Parser(allow_no_value=True, strict=False, interpolation=None)
+    parser.read_string(lines)
+    for section in parser.values():
+        yield from section.items()
+
+
+def _trans(stream: Iterable[Tuple[str, str]]) -> Iterator[Tuple[str, str]]:
+    legal = {*ascii_letters, digits, "_"}
+    seen = {**environ}
+    for key, val in stream:
+        if {*key} <= legal:
+            if parts := split(val):
+                part, *_ = parts
+                tpl = Template(part)
+                seen[key] = rhs = tpl.safe_substitute(seen)
+                yield key, rhs
 
 
 def _arg_parse() -> Namespace:
@@ -28,31 +47,14 @@ def _arg_parse() -> Namespace:
 async def main() -> int:
     args = _arg_parse()
     text = stdin.read() if args.stdin else Path(args.path).read_text()
-    lines = "".join(chain((f"[{uuid4()}]", linesep), text))
-    parser = _Parser(allow_no_value=True, strict=False, interpolation=None)
+    for lhs, rhs in _trans(_parse(text)):
+        line = f"{lhs}={rhs}"
+        stderr.write(line)
+        stderr.write(linesep)
+        stdout.write(f"export {line}")
+        stdout.write(linesep)
 
-    try:
-        parser.read_string(lines)
-    except ParsingError as e:
-        log.critical("%s", e)
-        return 1
-    else:
-        legal = {*ascii_letters, digits, "_"}
-        seen = {**environ}
-        for section in parser.values():
-            for key, val in section.items():
-                if {*key} <= legal:
-                    if parts := split(val):
-                        part, *_ = parts
-                        tpl = Template(part)
-                        seen[key] = rhs = tpl.safe_substitute(seen)
-                        line = f"{key}={rhs}"
-                        stderr.write(line)
-                        stderr.write(linesep)
-                        stdout.write(f"export {line}")
-                        stdout.write(linesep)
-
-        return 0
+    return 0
 
 
 run_main(main())
