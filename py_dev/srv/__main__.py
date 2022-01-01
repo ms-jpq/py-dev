0da86aa2ae87197e380baf7b9cf8eaa8bb7627b9
IIIIII <git@bigly.dog>
2022-01-01 01:12:10 -0800

    use create server :D

--- py_dev/srv/__main__.py
+++ py_dev/srv/__main__.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser, Namespace
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from http.server import BaseHTTPRequestHandler
+from ipaddress import ip_address
 from os import curdir
 from os.path import normcase
 from pathlib import Path, PurePath
-from socket import getfqdn
 from webbrowser import open as w_open
 
+from std2.http.server import create_server
 from std2.shutil import hr_print
 
 from ..log import log
@@ -17,16 +18,15 @@ def _parse_args() -> Namespace:
     cwd = PurePath(normcase(curdir))
     parser = ArgumentParser()
     parser.add_argument("root", nargs="?", type=PurePath, default=cwd)
-    parser.add_argument("-o", "--open", action="store_true")
-    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("-o", "--open", type=int, default=0)
     return parser.parse_args()
 
 
 async def main() -> int:
+
     args = _parse_args()
-    addr = "" if args.open else "localhost"
-    bind = (addr, args.port)
-    host = getfqdn() if args.open else "localhost"
+
+    bind = ("" if args.open else ip_address("::1"), args.open)
 
     j2 = build_j2()
     try:
@@ -39,16 +39,21 @@ async def main() -> int:
             def do_GET(self) -> None:
                 get(j2, handler=self, root=root)
 
-        httpd = ThreadingHTTPServer(bind, Handler)
+        httpd = create_server(bind, Handler)
     except OSError as e:
         log.fatal("%s", hr_print(e))
         return 1
     else:
-        encoded = host.encode("idna").decode()
-        location = f"http://{encoded}:{args.port}"
-        log.info("%s", hr_print(f"SERVING -- {location}"))
-        w_open(location)
-        httpd.serve_forever()
+        with httpd:
+            host = (
+                httpd.server_name.encode("idna").decode() if args.open else "localhost"
+            )
+            _, port, *_ = httpd.socket.getsockname()
+            location = f"http://{host}:{port}"
+            w_open(location)
+
+            log.info("%s", hr_print(f"SERVING -- {location}"))
+            httpd.serve_forever()
         return 0
 
 
