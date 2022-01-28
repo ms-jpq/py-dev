a4ce263c99f71e4321e858873b893ec705749960
IIIIII <github@bigly.dog>
2022-01-28 15:35:39 -0800

    ok

--- py_dev/git/time_machine/__main__.py
+++ py_dev/git/time_machine/__main__.py
@@ -60,7 +60,7 @@ async def _fzf_rhs(unified: int, sha: str, path: str) -> None:
         diff = await _git_show_diff(unified, sha=sha, path=path)
         await pretty_diff(diff, path=path)
     else:
-        content = await _git_show_file("{sha}~", path=path)
+        content = await _git_show_file(f"{sha}~", path=path)
         await pprn(content, path=path)
 
 
@@ -85,7 +85,7 @@ async def main() -> int:
         await _fzf_rhs(args.unified, sha=sha, path=args.path)
     elif args.execute:
         sha, _, _ = Path(args.execute).read_text().rstrip("\0").partition(" ")
-        await call("git", "show", f"{sha}:{args.path}")
+        await call("git", "show", f"{sha}~:{args.path}")
     else:
         commits = await _git_file_log(args.path)
         await _fzf_lhs(args.unified, path=args.path, commits=commits)
